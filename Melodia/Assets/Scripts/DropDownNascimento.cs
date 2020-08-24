a69f822326f3746716ac025040b5c6d716cc23da
Marcelo Vassoler Dias <vassoledias@hotmail.com>
2020-08-24 17:09:45 -0300

    tela cadastro

---
+++ Melodia/Assets/Scripts/DropDownNascimento.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class DropDownNascimento : MonoBehaviour
+{
+    public Dropdown dia;
+    public Dropdown mes;
+    public Dropdown ano;
+    // Start is called before the first frame update
+    void Start()
+    {
+        if(dia != null)
+        {
+            dia = GetComponent<Dropdown>();
+            dia.ClearOptions();
+            dia.AddOptions(iniciarDropDown("dia"));
+        }
+       if(mes != null)
+        {
+            mes = GetComponent<Dropdown>();
+            mes.ClearOptions();
+            mes.AddOptions(iniciarDropDown("mes"));
+        }
+       if(ano != null)
+        {
+            ano = GetComponent<Dropdown>();
+            ano.ClearOptions();
+            ano.AddOptions(iniciarDropDown("ano"));
+        }
+       
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    List<string> iniciarDropDown(string tipo)
+    {
+        List<string> options = new List<string>();
+        switch (tipo)
+        {
+            case "dia":
+                for(int i = 1; i <= 31; i++)
+                {
+                    options.Add(i.ToString());
+                }
+                break;
+            case "mes":
+                for (int i = 1; i <= 12; i++)
+                {
+                    options.Add(i.ToString());
+                }
+                break;
+            case "ano":
+                for (int i = DateTime.Now.Year; i >= 1900 ; i--)
+                {
+                    options.Add(i.ToString());
+                }
+                break;
+        }
+        return options;
+    }
+}
--- Melodia/Assets/Scripts/LoginScript.cs
+++ Melodia/Assets/Scripts/LoginScript.cs
@@ -4,45 +4,26 @@ using UnityEngine;
 using Mono.Data.Sqlite;
 using System.Data;
 using System;
-using UnityEngine.UI;
 using UnityEngine;
+using UnityEngine.UI;
 using UnityEngine.SceneManagement;
 
 public class LoginScript : MonoBehaviour
 {
     public InputField login;
     public InputField senha;
+    public Dropdown sexo;
+    public Dropdown dia;
+    public Dropdown mes;
+    public Dropdown ano;
+    public Text retornoTelaLogin;
     public string sceneName;
 
     private void Start()
     {
-        ReadCharacters();
+        retornoTelaLogin.enabled = false;
     }
 
-    private void ReadCharacters()
-    {
-        string connectionString = "URI=file:" + Application.dataPath + "/melodia_database.db";
-
-        using (var connection = new SqliteConnection(connectionString))
-        {
-            connection.Open();
-            using (var command = connection.CreateCommand())
-            {
-                command.CommandText = "SELECT * FROM login";
-                command.CommandType = CommandType.Text;
-
-                using (var reader = command.ExecuteReader())
-                {
-                    while (reader.Read())
-                    {
-                        print((string)reader[1] + "\n" +
-                              (int)reader[2] + "\n" +
-                              (string)reader[3]);
-                    }
-                }
-            }
-        }
-    }
 
     private bool Login()
     {
@@ -81,11 +62,32 @@ public class LoginScript : MonoBehaviour
         {
             SceneManager.LoadScene(sceneName);
         }
+        else
+        {
+            this.retornoTelaLogin.enabled = true;
+            this.retornoTelaLogin.text = "Login ou senha inválidos";
+        }
     }
 
-    public void Save()
+    public void SalvarJogador()
     {
         string connectionString = "URI=file:" + Application.dataPath + "/melodia_database.db";
+
+        List<Dropdown.OptionData> menuOptions = this.sexo.GetComponent<Dropdown>().options;
+        int menuIndex = this.sexo.GetComponent<Dropdown>().value;
+        string sexo = menuOptions[menuIndex].text;
+
+        menuOptions = this.dia.GetComponent<Dropdown>().options;
+        menuIndex = this.dia.GetComponent<Dropdown>().value;
+        string dia_nascimento = menuOptions[menuIndex].text;
+
+        menuOptions = this.mes.GetComponent<Dropdown>().options;
+        menuIndex = this.mes.GetComponent<Dropdown>().value;
+        string mes_nascimento = menuOptions[menuIndex].text;
+
+        menuOptions = this.ano.GetComponent<Dropdown>().options;
+        menuIndex = this.ano.GetComponent<Dropdown>().value;
+        string ano_nascimento = menuOptions[menuIndex].text;
 
         string pLogin = this.login.text;
         string pSenha = this.senha.text;
@@ -100,20 +102,40 @@ public class LoginScript : MonoBehaviour
                 {
                     try
                     {
-                        command.CommandText = "INSERT INTO login (login,senha) VALUES (@login, @senha);";
+                        command.CommandText = "INSERT INTO jogador (sexo,dia_nascimento,mes_nascimento,ano_nascimento) VALUES (@sexo, @dia, @mes, @ano);";
                         command.CommandType = CommandType.Text;
-                        command.Parameters.Add(new SqliteParameter("login", pLogin));
-                        command.Parameters.Add(new SqliteParameter("senha", pSenha));                        
+                        command.Parameters.Add(new SqliteParameter("sexo", sexo));
+                        command.Parameters.Add(new SqliteParameter("dia", dia_nascimento));
+                        command.Parameters.Add(new SqliteParameter("mes", mes_nascimento));
+                        command.Parameters.Add(new SqliteParameter("ano", ano_nascimento));
 
                         command.Transaction = transaction;
 
                         var rows = command.ExecuteNonQuery();
 
-                        transaction.Commit();                       
+                        command.CommandText = "select last_insert_rowid();";
+
+                        Int64 fkId64 = (Int64)command.ExecuteScalar();
+                        int fkId = (int)fkId64;
+
+                        command.CommandText = "INSERT INTO login (login,senha,jogador_id) VALUES (@login, @senha,@fkid);";
+                        command.CommandType = CommandType.Text;
+                        command.Parameters.Add(new SqliteParameter("login", pLogin));
+                        command.Parameters.Add(new SqliteParameter("senha", pSenha));
+                        command.Parameters.Add(new SqliteParameter("fkid", fkId));
+
+                        command.Transaction = transaction;
+
+                        rows = command.ExecuteNonQuery();
+
+                        transaction.Commit();
+                        SceneManager.LoadScene(sceneName);
                     }
                     catch (Exception e)
                     {
                         transaction.Rollback();
+                        this.retornoTelaLogin.enabled = true;
+                        this.retornoTelaLogin.text = "Esse login não está disponível";
                         print(e.Message);
                     }
                 }
